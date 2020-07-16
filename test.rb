3339c854632556770d2c725d101070bac04ac510
Moe <moe_akahane@diveintocode.jp>
2020-07-16 11:24:30 +0900

    再提出

--- test.rb
+++ test.rb
@@ -8,7 +8,7 @@ server = WEBrick::HTTPServer.new({
   Signal.trap(signal){ server.shutdown }
 }
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
-server.mount('/top', WEBrick::HTTPServlet::ERBHandler, 'top.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'top.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.start
